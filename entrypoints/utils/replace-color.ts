51dc39ed800224fca483abc52044c9e7890b5eff
suancaiyu1806 <suancaiyudgz@foxmail.com>
2024-08-08 20:08:49 +0800

    feat: 普通图片支持转变颜色

--- entrypoints/utils/replace-color.ts
+++ entrypoints/utils/replace-color.ts
@@ -50,10 +50,32 @@ function replaceCommonColor(elements: NodeListOf<HTMLElement>) {
 }
 
 function replaceImgColor(elements: NodeListOf<HTMLElement>) {
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
   // 遍历所有图片元素，识别并修改属性中的颜色字段
   elements.forEach((element) => {
     if (element.nodeName === "IMG") {
-      //TODO: 处理图片重写src
+      const img = element as HTMLImageElement;
+      if (img.src) {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx?.drawImage(img, 0, 0);
+        const imageData = ctx?.getImageData(0, 0, canvas.width, canvas.height);
+        if (imageData) {
+          const data = imageData.data;
+          for (let i = 0; i < data.length; i += 4) {
+            const averageData = (data[i] + data[i + 1] + data[i + 2]) / 3;
+            data[i] = averageData;
+            data[i + 1] = averageData;
+            data[i + 2] = averageData;
+          }
+          ctx?.putImageData(imageData, 0, 0);
+
+          // 将修改后的图像替换 img 元素
+          img.src = canvas.toDataURL();
+          img.srcset = ""; // TODO: 后续可以优化下 srcset 的情况
+        }
+      }
     }
     // FIXME: SVG 可能会有坑
     if (element.nodeName === "svg") {
